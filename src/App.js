97ff8dc8bc0c86b3917e9bf4f7d9d08ef0560877
Trent Slutzky <trent.slutzky@gmail.com>
2022-06-04 23:45:54 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import styled from "styled-components";
+import moment from "moment";
 
-function App() {
+import BG from "./wallpaper.jpg";
+
+export default function App() {
+
+  const [time_text, setTimeText] = useState("");
+  const [date_text, setDateText] = useState("");
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const today = new Date();
+      console.log(moment().format("h:mm"));
+      setTimeText(moment().format("h:mm"));
+      setDateText(moment().format("dddd, MMMM Do"));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <TimeParagraph>{time_text}</TimeParagraph>
+      <DateParagraph>{date_text}</DateParagraph>
+    </MainContainer>
   );
 }
 
-export default App;
+const MainContainer = styled.div`
+  background:#1a2c37;
+  width:100%;
+  height:100%;
+  padding:1em;
+  background-image:url(${BG});
+`;
+
+const DateParagraph = styled.p`
+  font-size:2em;
+  text-align:center;
+  margin-top:5em;
+`;
+
+const TimeParagraph = styled.p`
+  font-size:19em;
+  margin-bottom:-0.85em;
+  margin-top:0.3em;
+  text-align:center;
+`;
